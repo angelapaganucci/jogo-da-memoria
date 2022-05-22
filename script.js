322ed7f7675692a6941e2c8d93f9f4897c4b8286
Angela Paganucci <angela.luiz@db1.com.br>
2022-05-21 22:31:52 -0300

    Sexta aula

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let lockBoard = false
 
 function flipCard() {
   if (lockBoard) return
+  if (this === firstCard) return
 
   this.classList.add('flip')
   if (!hasFlippedCard) {
@@ -29,13 +30,24 @@ function checkForMath() {
 function disableCards() {
   firstCard.removeEventListener('click', flipCard)
   secondtCard.removeEventListener('click', flipCard)
+
+  resetBoard()
 }
 
 function unflipCards() {
+  lockBoard = true
+
   setTimeout(() => {
     firstCard.classList.remove('flip')
     secondCard.classList.remove('flip')
+
+    resetBoard()
   }, 1500)
+}
+
+function resetBoard() {
+  ;[hasFlippedCard, lockBoard] = [false, false]
+  ;[firstCard, secondCard] = [null, null]
 }
 
 cards.forEach((card) => {
